76dbe82100a0085e10024793d0f8bc9b376b5075
Erdem Bayer <ebayer@bayer.gen.tr>
2012-11-27 22:39:27 +0200

    Fix typo

--- kurs/urls.py
+++ kurs/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('kurs.views',
     url(r'^$', 'index'),
-    url(r'^events/$', 'list_events'),
-    url(r'^event/(?P<event_id>\d+)/$', 'list_courses'),
+    url(r'^etkinlik/$', 'list_events'),
+    url(r'^etkinlik/(?P<event_id>\d+)/$', 'list_courses'),
     url(r'^kurs/(?P<course_id>\d+)/$', 'course_details'),
 )
