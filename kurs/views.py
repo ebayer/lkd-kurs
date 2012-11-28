bb3e1a0a65ed88f0d8ab82312ad0db679765498a
Erdem Bayer <ebayer@bayer.gen.tr>
2012-11-28 13:32:10 +0200

    Use shortcuts

--- kurs/views.py
+++ kurs/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.template import RequestContext
 from kurs.models import *
 
 def index(request):
-    return render_to_response('kurs/index.html', {}, context_instance=RequestContext(request))
+    return render_to_response('kurs/index.html',
+                              {},
+                              context_instance=RequestContext(request))
 
 def list_events(request):
     event_list = Event.objects.all().order_by('-display_name')
@@ -14,11 +15,8 @@ def list_events(request):
                               context_instance=RequestContext(request))
     
 def list_courses(request, event_id):
-    try:
-        event = Event.objects.get(pk=event_id)
-        course_list = Course.objects.filter(event=event_id)
-    except Event.DoesNotExist or Course.DoesNotExist:
-        raise Http404
+    event = get_object_or_404(Event, pk=event_id)
+    course_list = get_list_or_404(Course, event=event_id)
     
     return render_to_response('kurs/list_courses.html',
                               {'event': event.display_name,
@@ -26,10 +24,7 @@ def list_courses(request, event_id):
                               context_instance=RequestContext(request))
     
 def course_details(request, course_id):
-    try:
-        course = Course.objects.get(pk=course_id)
-    except Course.DoesNotExist:
-        raise Http404
+    course = get_object_or_404(Course, pk=course_id)
     
     return render_to_response('kurs/course_details.html',
                               {'course': course,},
