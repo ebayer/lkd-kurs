250b5f2cb592d158206e80a4cc9c029ff3e5c805
Erdem Bayer <ebayer@bayer.gen.tr>
2013-02-23 18:19:54 +0200

    Fix login link to redirect to previous page after successful login
    fixes #1

--- lkd/settings.py
+++ lkd/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/mnt/yedek/ebayer/yedek/workspace/dev/eclipse_projects/lkd/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/mnt/yedek/ebayer/yedek/workspace/repos/git/lkd-kurs/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -45,12 +45,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/ebayer/django-media-files/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -67,7 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/ebayer/workspace/dev/eclipse_projects/lkd/kurs/static',
+    '/home/ebayer/workspace/repos/git/lkd-kurs/kurs/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -107,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/ebayer/workspace/dev/eclipse_projects/lkd/kurs/templates',
+    '/home/ebayer/workspace/repos/git/lkd-kurs/kurs/templates',
 )
 
 INSTALLED_APPS = (
@@ -121,11 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    #'registration',
-    'userena',
-    'guardian',
-    'easy_thumbnails',
-    #'profiles',
+    'registration',
+    'profiles',
     'kurs',
 )
 
@@ -164,18 +161,22 @@ ACCOUNT_ACTIVATION_DAYS = 7
 LOGIN_REDIRECT_URL='/kurs/'
 REGISTRATION_OPEN=True
 
-# These settings are for django-userena
-AUTHENTICATION_BACKENDS = (
-    'userena.backends.UserenaAuthenticationBackend',
-    'guardian.backends.ObjectPermissionBackend',
-    'django.contrib.auth.backends.ModelBackend',
+# Dummy SMTP Server
+# use the following command to test email
+# python -m smtpd -n -c DebuggingServer localhost:1025
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
 )
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-ANONYMOUS_USER_ID = -1
-LOGIN_REDIRECT_URL = '/kurs/'
-LOGIN_URL = '/accounts/signin/'
-LOGOUT_URL = '/accounts/signout/'
-USERENA_ACTIVATION_REQUIRED=False
-USERENA_DEFAULT_PRIVACY='closed'
-USERENA_DISABLE_PROFILE_LIST=True
-USERENA_HIDE_EMAIL=True
